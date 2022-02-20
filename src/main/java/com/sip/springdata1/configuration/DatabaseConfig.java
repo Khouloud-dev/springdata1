8c96449d66c9749cb0c0a0da9e6b9688ee4d2201
Khoulouda <kboubakri19@gmail.com>
2022-02-20 08:16:49 +0100

    Postgres config

---
+++ src/main/java/com/sip/springdata1/configuration/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package com.sip.springdata1.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Bean
+  public DataSource dataSource() {
+      HikariConfig config = new HikariConfig();
+      config.setJdbcUrl(dbUrl);
+      return new HikariDataSource(config);
+  }
+}
